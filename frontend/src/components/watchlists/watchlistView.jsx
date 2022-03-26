a7364d540d6011f94abf08a601f3954626976d58
Adrian Jendo <ajendo@uwaterloo.ca>
2022-03-25 20:45:42 -0700

    Delete useStyles

--- frontend/src/components/watchlists/watchlistView.jsx
+++ frontend/src/components/watchlists/watchlistView.jsx
@@ -128,27 +128,6 @@ const defaultColumns = [
 // 	},
 // ];
 
-// const useStyles = makeStyles((theme) => ({
-// 	root: {
-// 		width: "100%",
-// 	},
-// 	paper: {
-// 		width: "100%",
-// 		marginBottom: theme.spacing(2),
-// 	},
-// 	visuallyHidden: {
-// 		border: 0,
-// 		clip: "rect(0 0 0 0)",
-// 		height: 1,
-// 		margin: -1,
-// 		overflow: "hidden",
-// 		padding: 0,
-// 		position: "absolute",
-// 		top: 20,
-// 		width: 1,
-// 	},
-// }));
-
 const StyledTableCell = styled(TableCell)(() => ({
     cursor: "default",
 }));
