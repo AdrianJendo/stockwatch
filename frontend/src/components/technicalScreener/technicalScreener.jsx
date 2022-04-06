051c225c8a32a44063b74187215d490555a29ca5
Adrian Jendo <ajendo@uwaterloo.ca>
2022-04-05 20:52:03 -0700

    Update dropdown

--- frontend/src/components/technicalScreener/technicalScreener.jsx
+++ frontend/src/components/technicalScreener/technicalScreener.jsx
@@ -12,75 +12,93 @@ import { technicalPatterns } from "constants";
 import axios from "axios";
 
 const TechnicalScreener = () => {
-    const [age, setAge] = useState("");
+    const [pattern, setPattern] = useState("");
     const [stocks, setStocks] = useState([]);
+    const [loadingResponse, setLoadingResponse] = useState(false);
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
+    const handleChange = (e) => {
+        setPattern(e.target.value);
     };
 
     const submitPattern = async () => {
-        console.log(age);
-        console.log(technicalPatterns);
+        setLoadingResponse(true);
         const resp = await axios.get("/api/technical_screener", {
-            params: { pattern: "CDLENGULFING" },
+            params: { pattern: technicalPatterns[pattern] },
         });
-        console.log(resp);
+        if (!resp.data.length) {
+            alert("NO DATA");
+        }
+        setLoadingResponse(false);
         setStocks(resp.data);
     };
 
     return (
         <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={age}
-                    label="Age"
-                    onChange={handleChange}
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+            <FormControl
+                sx={{ display: "flex", margin: "30px auto", width: "200px" }}
+            >
+                <InputLabel>Pattern</InputLabel>
+                <Select value={pattern} label="Pattern" onChange={handleChange}>
+                    {Object.keys(technicalPatterns).map((pattern) => (
+                        <MenuItem key={pattern} value={pattern}>
+                            {pattern}
+                        </MenuItem>
+                    ))}
                 </Select>
             </FormControl>
-            <Button onClick={() => submitPattern()} variant="contained">
+            <Button
+                sx={{ display: "flex", margin: "30px auto" }}
+                onClick={() => submitPattern()}
+                variant="contained"
+            >
                 Submit
             </Button>
 
-            <div style={{ overflow: "scroll", height: "70vh" }}>
-                {stocks.map((stock) => (
-                    <Box
-                        sx={{
-                            display: "flex",
-                            padding: "20px",
-                            alignItems: "center",
-                        }}
-                    >
-                        <img
-                            src={`https://finviz.com/chart.ashx?t=${stock.ticker}&ty=c&ta=1&p=d&s=l`}
-                        />
-
-                        <Typography
+            {loadingResponse ? (
+                <Typography
+                    sx={{
+                        padding: "50px",
+                        textAlign: "center",
+                    }}
+                    variant="h2"
+                >
+                    Loading...
+                </Typography>
+            ) : (
+                <div style={{ overflowY: "auto", height: "650px" }}>
+                    {stocks.map((stock) => (
+                        <Box
                             sx={{
-                                margin: "auto 0px",
-                                marginLeft: "60px",
-                                padding: "30px",
-                                backgroundColor:
-                                    stock.sentiment === "bullish"
-                                        ? "green"
-                                        : "red",
-                                height: "24px",
-                                width: "100px",
-                                textAlign: "center",
+                                display: "flex",
+                                padding: "20px",
+                                alignItems: "center",
                             }}
+                            key={stock.ticker}
                         >
-                            {stock.sentiment}
-                        </Typography>
-                    </Box>
-                ))}
-            </div>
+                            <img
+                                src={`https://finviz.com/chart.ashx?t=${stock.ticker}&ty=c&ta=1&p=d&s=l`}
+                            />
+
+                            <Typography
+                                sx={{
+                                    margin: "auto",
+                                    marginLeft: "60px",
+                                    padding: "30px",
+                                    backgroundColor:
+                                        stock.sentiment === "bullish"
+                                            ? "green"
+                                            : "red",
+                                    height: "24px",
+                                    width: "100px",
+                                    textAlign: "center",
+                                }}
+                            >
+                                {stock.sentiment}
+                            </Typography>
+                        </Box>
+                    ))}
+                </div>
+            )}
         </Box>
     );
 };
